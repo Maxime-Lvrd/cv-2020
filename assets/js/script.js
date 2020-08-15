3aaf78fb1387cc019cea2deabf7d31dd40e46a70
mak <lievrard.maxime@icloud.com>
2020-08-15 18:47:17 +0200

    cv 2020

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/*
+window.onload = function() {
+    let nav = document.querySelector('nav ul');
+if (window.innerWidth < 675) {
+    nav.classList.remove("row");
+} else {
+    nav.classList.add("row");
+}
+}
+*/
